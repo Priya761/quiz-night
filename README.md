85b8e3211e770c5539d93a48065e1fe47c8ce696
priyajha <priyajha.tiim.18@gmail.com>
2023-04-02 14:04:35 +0530

    Initialized quiz-night application

--- README.md
+++
@@ -1,2 +0,0 @@
-# quiz-night
-To create a quiz application using React JS as the front-end.
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <p className='text-2xl font-bold text-red-500'>Hello World!</p>
+    </div>
+  )
+}
+
+export default App
